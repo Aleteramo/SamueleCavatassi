50b2fc63360264772365e2121552588d8ec103c9
Aleteramo <157708915+Aleteramo@users.noreply.github.com>
2024-02-29 17:33:49 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
-// Esempio con importazioni di moduli ES6
-import './scrollManagement.js';
-import './animations.js';
-
-// Codice di inizializzazione generale
+// main.js
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Il sito è completamente caricato e pronto.');
-    // Qui potresti chiamare funzioni di inizializzazione o setup iniziali, se necessario.
+    // Eventuale codice di inizializzazione qui
 });

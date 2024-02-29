d7a1fc283d625f3cb54d85fe4dcc621eda327ea5
Aleteramo <157708915+Aleteramo@users.noreply.github.com>
2024-02-29 17:31:17 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Esempio con importazioni di moduli ES6
+import './scrollManagement.js';
+import './animations.js';
+
+// Codice di inizializzazione generale
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Il sito è completamente caricato e pronto.');
+    // Qui potresti chiamare funzioni di inizializzazione o setup iniziali, se necessario.
+});
